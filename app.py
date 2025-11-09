2dbd2d0b23dfffcad9ed762e79371a4c129a5e29
Harshit Dubey <harshitdubey11578@gmail.com>
2025-11-09 10:44:13 +0530

    Refactor fake news detection and update metadata
    Refactor fake news detection logic and update author information.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-# @Abhishek pp
-# github-abiek12
-
+# @Harshit
+# github - harshitdubey0
 
 from flask import Flask, render_template, request
 import pandas as pd
@@ -16,55 +15,52 @@ from matplotlib import pyplot as plt
 from sklearn.linear_model import PassiveAggressiveClassifier
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+import os
 
-app = Flask(__name__,template_folder='./templates',static_folder='./static')
+# Ensure stopwords are available
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
+
+# Load model and vectorizer
 loaded_model = pickle.load(open("model.pkl", 'rb'))
 vector = pickle.load(open("vector.pkl", 'rb'))
+
 lemmatizer = WordNetLemmatizer()
 stpwrds = set(stopwords.words('english'))
-corpus = []
 
 def fake_news_det(news):
-    review = news
-    review = re.sub(r'[^a-zA-Z\s]', '', review)
+    review = re.sub(r'[^a-zA-Z\s]', '', news)
     review = review.lower()
     review = nltk.word_tokenize(review)
-    corpus = []
-    for y in review :
-        if y not in stpwrds :
-            corpus.append(lemmatizer.lemmatize(y))
+    corpus = [lemmatizer.lemmatize(word) for word in review if word not in stpwrds]
     input_data = [' '.join(corpus)]
     vectorized_input_data = vector.transform(input_data)
     prediction = loaded_model.predict(vectorized_input_data)
-     
     return prediction
-
-        
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         message = request.form['news']
         pred = fake_news_det(message)
-        print(pred)
-        def predi(pred):
-            if pred[0] == 1:
-              res="Prediction of the News :  Looking Fake News📰"
-            else:
-              res="Prediction of the News : Looking Real News📰 "
-            return res
-        result=predi(pred)
-        return render_template("prediction.html",  prediction_text="{}".format(result))
+
+        if pred[0] == 1:
+            result = "Prediction of the News : Looking Fake News 📰"
+        else:
+            result = "Prediction of the News : Looking Real News 📰"
+
+        return render_template("prediction.html", prediction_text=result)
     else:
-        return render_template('prediction.html', prediction="Something went wrong")
-
-
+        return render_template('prediction.html', prediction_text="Something went wrong")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
